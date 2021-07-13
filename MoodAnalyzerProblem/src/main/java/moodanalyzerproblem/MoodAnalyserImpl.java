a099f4273606278149968edaadabddfe1b98afc9
SanketTambe39 <sankettambe39@gmail.com>
2021-07-14 00:04:15 +0530

    MODIFIED: refactored to the mood msg in constructor

--- MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyserImpl.java
+++ MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyserImpl.java
@@ -3,26 +3,25 @@ package moodanalyzerproblem;
 public class MoodAnalyserImpl implements MoodAnalyserService {
 
 	public String message;
+
 	public MoodAnalyserImpl() {
 		super();
 	}
+
 	public MoodAnalyserImpl(String message) {
 		super();
 		this.message = message;
 	}
-	public Object analyseMood()
-	{
+
+	public Object analyseMood() {
 		return analyseMood(message);
 	}
+
 	@Override
-	public String analyseMood(String message)
-	{
-		if(message.contains("sad"))
-		{
+	public String analyseMood(String message) {
+		if (message.contains("sad")) {
 			return "sad";
-		}
-		else
-		{
+		} else {
 			return "happy";
 		}
 	}
--- MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyzerMain.java
+++ MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyzerMain.java
@@ -3,8 +3,8 @@ package moodanalyzerproblem;
 public class MoodAnalyzerMain {
 
 	public static void main(String[] args) {
-		MoodAnalyserService moodAnalyser = new MoodAnalyserImpl();
 		String message = "Today i am very sad";
+		MoodAnalyserService moodAnalyser = new MoodAnalyserImpl(message);
 		System.out.println(moodAnalyser.analyseMood(message));
 	}
 

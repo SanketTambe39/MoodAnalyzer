da623166edf09bb80b6b1bad0b0bea50332f75ea
SanketTambe39 <sankettambe39@gmail.com>
2021-07-13 23:37:39 +0530

    ADDED: Method to check sad or happy

--- MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyserImpl.java
+++ MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyserImpl.java
@@ -1,6 +1,16 @@
 package moodanalyzerproblem;
 
-public class MoodAnalyserImpl  {
+public class MoodAnalyserImpl implements MoodAnalyserService {
 
-
+	@Override
+	public String analyseMood(String message) {
+		if(message.contains("sad"))
+		{
+			return "sad";
+		}
+		else
+		{
+			return "happy";
+		}
+	}
 }
--- MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyzerMain.java
+++ MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyzerMain.java
@@ -3,7 +3,9 @@ package moodanalyzerproblem;
 public class MoodAnalyzerMain {
 
 	public static void main(String[] args) {
-		
+		MoodAnalyserService moodAnalyser = new MoodAnalyserImpl();
+		String message = "Today i am very sad";
+		System.out.println(moodAnalyser.analyseMood(message));
 	}
 
 }

a2f26c0f4dc952d6132109b80d5eaeb3192e4c5c
SanketTambe39 <sankettambe39@gmail.com>
2021-07-13 23:53:37 +0530

    Added : Test cases for passing data

--- MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyserImpl.java
+++ MoodAnalyzerProblem/src/main/java/moodanalyzerproblem/MoodAnalyserImpl.java
@@ -2,8 +2,21 @@ package moodanalyzerproblem;
 
 public class MoodAnalyserImpl implements MoodAnalyserService {
 
+	public String message;
+	public MoodAnalyserImpl() {
+		super();
+	}
+	public MoodAnalyserImpl(String message) {
+		super();
+		this.message = message;
+	}
+	public Object analyseMood()
+	{
+		return analyseMood(message);
+	}
 	@Override
-	public String analyseMood(String message) {
+	public String analyseMood(String message)
+	{
 		if(message.contains("sad"))
 		{
 			return "sad";
--- MoodAnalyzerProblem/src/test/java/moodanalyzerproblemtesting/MoodAnalysisTest.java
+++ MoodAnalyzerProblem/src/test/java/moodanalyzerproblemtesting/MoodAnalysisTest.java
@@ -9,11 +9,27 @@ import moodanalyzerproblem.MoodAnalyserService;
 public class MoodAnalysisTest {
 
 	MoodAnalyserService moodService = new MoodAnalyserImpl();
+
 	//check for sad mood
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad() 
 	{
 		Assert.assertEquals("sad",moodService.analyseMood("i am in sad mood"));
+	}
+	
+	//check for happy mood
+	@Test
+	public void givenMessage_WhenNoMoodSpecified_ShouldReturnhappy() 
+	{
+		Assert.assertEquals("happy",moodService.analyseMood("i am in any mood"));
+	}
+	
+	//check for sad mood and message passed through constructor
+	@Test
+	public void givenMessageInConstructor_WhenSad_ShouldReturnSad()
+	{
+		MoodAnalyserImpl moodAnalyse = new MoodAnalyserImpl("i am in sad mood");
+		Assert.assertEquals("sad",moodAnalyse.analyseMood());
 	}
 	
 }

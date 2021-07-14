76d4d3671c59b13c93e3a4c4790c09edecb15cf4
SanketTambe39 <sankettambe39@gmail.com>
2021-07-14 10:28:32 +0530

    MODIFIED: Exception Handled for null pointer

--- MoodAnalyzerProblem/src/test/java/moodanalyzerproblemtesting/MoodAnalysisTest.java
+++ MoodAnalyzerProblem/src/test/java/moodanalyzerproblemtesting/MoodAnalysisTest.java
@@ -8,16 +8,16 @@ import moodanalyzerproblem.MoodAnalyserImpl;
 import moodanalyzerproblem.MoodAnalyserService;
 
 public class MoodAnalysisTest {
-	MoodAnalyserService moodService = new MoodAnalyserImpl();
-	MoodAnalyserImpl moodAnalyse = new MoodAnalyserImpl(null);		
+	
+			
 	//check for sad mood
 		@Test
 		public void givenMessage_WhenSad_ShouldReturnSad() 
 		{
+			MoodAnalyserService moodService = new MoodAnalyserImpl();
 			try {
 				Assert.assertEquals("sad",moodService.analyseMood("i am in sad mood"));
 			} catch (MoodAnalyseException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -26,10 +26,10 @@ public class MoodAnalysisTest {
 		@Test
 		public void givenMessage_WhenNoMoodSpecified_ShouldReturnhappy() 
 		{
+			MoodAnalyserService moodService = new MoodAnalyserImpl();
 			try {
 				Assert.assertEquals("happy",moodService.analyseMood("i am in any mood"));
 			} catch (MoodAnalyseException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -42,7 +42,6 @@ public class MoodAnalysisTest {
 			try {
 				Assert.assertEquals("sad",moodAnalyse.analyseMood());
 			} catch (MoodAnalyseException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -51,6 +50,7 @@ public class MoodAnalysisTest {
 		@Test
 		public void givenMessageInConstructor_WhenHappy_ShouldReturnHappy()
 		{
+			MoodAnalyserImpl moodAnalyse = new MoodAnalyserImpl(null);
 			try {
 				Assert.assertEquals("happy",moodAnalyse.analyseMood());
 			} catch (MoodAnalyseException e) {
@@ -59,16 +59,17 @@ public class MoodAnalysisTest {
 		}
 
 		//check for null message passed through constructor
-		//check for null message passed through constructor
 		@Test
 		public void givenMessageInConstructor_WhenNull_ShouldReturnHappy()
 		{
-			MoodAnalyserImpl moodAnalyse = new MoodAnalyserImpl(null);		
-			try {
-				Assert.assertEquals("happy",moodAnalyse.analyseMood());
-			} catch (MoodAnalyseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			try 
+			{
+				MoodAnalyserImpl moodAnalyse = new MoodAnalyserImpl(null);		
+				Assert.assertEquals("happy",moodAnalyse.analyseMood());			
+			} 
+			catch (MoodAnalyseException e) 
+			{
+				Assert.assertEquals("message cannot be null", e.getMessage());
 			}
 		}
 }
